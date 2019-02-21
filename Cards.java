b82402fd5db2d046809a0a38cde6025a6cbf8269
CaHerm <cam2000h@gmail.com>
2019-02-21 03:04:54 -0700

    Utility Class for handling Cards
    Simple file system for creating and accessing Cards. See cards.txt for how the formatting works.
    I tried to be as detailed as I could in commenting the unclear parts.
    As an example, we could say: new Deck(Cards.get("Strike"), Cards.get("block"));

---
+++ Cards.java
@@ -0,0 +1,64 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * Holds and allows access to all Card objects. the load method must be called exactly once for this classes' other methods to be used without error.
+ */
+public class Cards {
+
+	private static ArrayList<Card> cards;
+	
+	/**
+	 * Initializes all Card objects as found in cards.txt. This method must be run once before other methods in Cards are used.
+	 */
+	public static void load () {
+		cards = new ArrayList<Card>();
+		//I have to catch errors(or throw the error) when working with files because java says so.
+		try {
+			//Similar to reading user input, this Scanner reads from a file.
+			Scanner read = new Scanner(new File("cards.txt"));
+			//Loops until the end of the file is reached.
+			while (read.hasNextLine()) {
+				String[] traits = read.nextLine().split(",");
+				cards.add(new Card(Integer.parseInt(traits[1]), Integer.parseInt(traits[2]), Integer.parseInt(traits[3]), Integer.parseInt(traits[4]), traits[0]));
+			}
+			//Leaving the Scanner open can cause the file to remain in use and become inaccessible.
+			read.close();
+			
+		} catch (IOException e) {
+			//Prints the source of error if the 'try' section of the try-catch encountered any.
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * @param name name of the card to find.
+	 * @return a Card object with the given name, or null if no such card exists.
+	 */
+	public static Card get (String name) {
+		for (Card c : cards) {
+			if (c.getName().equals(name))
+				return c;
+		}
+		return null;
+	}
+	
+	/**
+	 * @return a random Card object.
+	 */
+	public static Card getRandom () {
+		//See: https://dzone.com/articles/random-number-generation-in-java
+		return cards.get((int) (Math.random() * cards.size()));
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		load();
+		System.out.println(getRandom().getDescription());
+		System.out.println(get("Block").getDescription());
+	}
+
+}
