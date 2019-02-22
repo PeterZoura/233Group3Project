272189ce6b97f676462d8f167677681fed0259d3
CaHerm <cam2000h@gmail.com>
2019-02-22 02:56:09 -0700

    Very rough Game class
    This really sucks, its buggy and ugly and needs a ton of work, but it's something for now.

---
+++ Game.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+
+public class Game {
+
+	public static void main(String[] args) {
+		Cards.load();
+		Scanner in = new Scanner(System.in);
+		System.out.println("What would you like to name your character?");
+		String name = in.nextLine();
+		Player player = new Player(name, 80, Cards.get("Strike"), Cards.get("Strike"), Cards.get("Strike"), Cards.get("Block"), Cards.get("Block"));
+		
+		while (player.alive()) {
+			
+			Monster monster = new Monster("Slime", 19, Cards.get("Over Powered"));
+			
+			player.startCombat();
+			while (monster.alive() && player.alive()) {
+				
+				System.out.println(monster.intentions());
+				
+				player.startTurn();
+				printStats(player, monster);
+				Card c;
+				while ((c = player.nextCard()) != null) {
+					c.use(player, monster);
+					printStats(player, monster);
+				}
+				player.endTurn();
+				
+				printStats(player, monster);
+				
+				monster.getMove().use(monster, player);
+				
+			}
+			
+			player.endCombat();
+			
+		}	
+		
+		in.close();
+	}
+	
+	public static void printStats(Entity player, Entity monster) {
+		System.out.println("\nHealth: " + player.getHealth() + "/" + player.getMaxHealth() + 
+				"      Energy: " + player.getEnergy() + "/" + player.getMaxEnergy()      + 
+				"      Armour: " + player.getArmour() + 
+				"\nMonster health: " + monster.getHealth() + "/" + monster.getMaxHealth() + 
+				"      Monster armour: " + monster.getArmour() + "\n");
+	}
+	
+}
