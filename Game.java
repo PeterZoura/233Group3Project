8fd1f54878d8b674437c07524ed6b306411cc4a0
CaHerm <cam2000h@gmail.com>
2019-02-24 01:05:55 -0700

    Game is fully functional, but fully ready for demo 1
    Has all the basic features, but is still really ugly and gross. From my testing, it seems to be bug-free.

--- Game.java
+++ Game.java
@@ -1,54 +1,97 @@
 import java.util.Scanner;
 
 
+/**
+ * Uses a main method to run the primary game loop.
+ */
 public class Game {
 
+	/**
+	 * Main game loop.
+	 */
 	public static void main(String[] args) {
+		
 		Cards.load();
+		
 		Scanner in = new Scanner(System.in);
-		System.out.println("What would you like to name your character?");
+		
+		System.out.println("Please enter your name:");
 		String name = in.nextLine();
-		Player player = new Player(name, 80, Cards.get("Strike"), Cards.get("Strike"), Cards.get("Strike"), Cards.get("Block"), Cards.get("Block"), Cards.get("Block"), Cards.get("Block"));
+		Player player = new Player(name, 10, Cards.get("Strike"), Cards.get("Strike"), Cards.get("Strike"), Cards.get("Block"), Cards.get("Block"), Cards.get("Block"), Cards.get("Block"));
+		
+		pressEnter(in, "Welcome, " + name + ", to Slay the Spire! In this brief demo, you will be challenged by enemies until you are defeated."
+				+ " Use your cards to attack and defend against the monsters!");
 		
 		while (player.alive()) {
 			
-			Monster monster = new Monster("Slime", 19, Cards.get("Over Powered"));
+			Monster monster = new Monster("Slime", 19, Cards.get("Monster Attack"), Cards.get("Monster Block"), Cards.get("Monster BlAttack"));
+			
+			System.out.println("An opponent has arrived: " + monster.getName());
 			
 			player.startCombat();
 			while (monster.alive() && player.alive()) {
 				
+				System.out.println(player.getName() + "'s turn!");
+				monster.setMove();
 				System.out.println(monster.intentions());
 				
+				player.setArmour(0);
 				player.startTurn();
 				printStats(player, monster);
-				Card c;
-				while ((c = player.nextCard()) != null) {
-					c.use(player, monster);
+				
+				while (player.nextCard(monster) && monster.alive()) {
+					System.out.println(monster.intentions());
 					printStats(player, monster);
 				}
+				
 				player.endTurn();
 				
-				printStats(player, monster);
-				
-				monster.getMove().use(monster, player);
-				monster.setArmour(0);
-				player.setArmour(0);
+				if (monster.alive() && player.alive()) {
+					pressEnter(in, player.getName() + "'s turn is over!");
+					
+					System.out.println(monster.getName() + "'s turn!");
+					monster.setArmour(0);
+					printStats(player, monster);
+					
+					monster.getMove().use(monster, player);
+					System.out.println(monster.actionReport());
+					printStats(player, monster);
+					pressEnter(in, "");
+				}
 				
 			}
 			
 			player.endCombat();
+			
+			if (!player.alive())
+				System.out.println("DEFEAT!");
+			else
+				pressEnter(in, monster.getName() + " has been slain!");
 			
 		}	
 		
 		in.close();
 	}
 	
+	/**
+	 * Prints the health and armour of the given player and monster, as well as the energy of the player.
+	 */
 	public static void printStats(Entity player, Entity monster) {
-		System.out.println("\n\n\n\n\n\nHealth: " + player.getHealth() + "/" + player.getMaxHealth() + 
-				"      Energy: " + player.getEnergy() + "/" + player.getMaxEnergy()      + 
-				"      Armour: " + player.getArmour() + 
-				"\nMonster health: " + monster.getHealth() + "/" + monster.getMaxHealth() + 
-				"      Monster armour: " + monster.getArmour() + "\n");
+		System.out.println("\n" + player.getName() + ":   health: " + player.getHealth() + "/" + player.getMaxHealth() + 
+				"      Energy: " + player.getEnergy() + "/"  + player.getMaxEnergy()     + 
+				"      Armour: " + player.getArmour() + "\n" + 
+				monster.getName() + ":    health: " + monster.getHealth() + "/" + monster.getMaxHealth() + 
+				"      Armour: " + monster.getArmour() + "\n");
+	}
+	
+	/**
+	 * Prompts the user to press enter, accompanied by a given message.
+	 * @param in Scanner to use to get the user's input.
+	 * @param message the message to display before "Press enter to continue."
+	 */
+	public static void pressEnter(Scanner in, String message) {
+		System.out.println(message + "\nPress enter to continue.");
+		in.nextLine();
 	}
 	
 }
