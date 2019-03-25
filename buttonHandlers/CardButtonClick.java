1365f6aae4a88adb5cce82582e486801d12e445d
CaHerm <cam2000h@gmail.com>
2019-03-24 19:14:01 -0600

    Project is now organized in packages

---
+++ buttonHandlers/CardButtonClick.java
@@ -0,0 +1,36 @@
+package buttonHandlers;
+import game.GameGUI;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import logic.Card;
+
+/**
+This class handles a button click and outputs a message.
+The handle method is invoked when the button is clicked.
+*/
+public class CardButtonClick extends GameGUI implements EventHandler<ActionEvent>
+{
+	int cardIndex;
+
+	public CardButtonClick(int cardIndex)
+	{
+		this.cardIndex = cardIndex;
+	}
+	@Override
+	public void handle(ActionEvent event)
+	{
+		
+		Card card = player.getDeck().getHand().get(cardIndex);
+		descriptions.setText(card.getDescription());
+		if (card.getCost()<=player.getEnergy()){
+			cardToUse = cardIndex;
+		}
+		else{
+			descriptions.setText("Not enough energy.");
+			cardToUse = -1;
+		}
+		potionToUse = -1;
+		//GameGUI.refreshVisuals();
+    }
+}
+
