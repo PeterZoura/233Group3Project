89203618db358632fade96068683b742cc24e56b
CaHerm <cam2000h@gmail.com>
2019-04-06 16:48:14 -0600

    Update MonsterTargetClick.java

--- buttonHandlers/MonsterTargetClick.java
+++ buttonHandlers/MonsterTargetClick.java
@@ -21,75 +21,75 @@ public class MonsterTargetClick extends GameGUI implements EventHandler<ActionEv
 	@Override
 	public void handle(ActionEvent event){
 	
-		if (cardToUse >= 0){
-			Card card = player.getDeck().getHand().get(cardToUse);
+		if (getCardToUse() >= 0){
+			Card card = getPlayer().getDeck().getHand().get(getCardToUse());
 			if ((card.getDamage()>0)){
 				if (!card.requiresTarget()){
-						card.use(player,combatMonsters);
+						card.use(getPlayer(),getCombatMonsters());
 					}
 					else{
-						card.use(player,combatMonsters[monsterIndex]);
+						card.use(getPlayer(),getCombatMonsters()[monsterIndex]);
 					}
-					cardButtons.get(cardToUse).setText("");
-					cardButtons.get(cardToUse).setVisible(false);
-					cardToUse = -1;	
+					getCardButtons().get(getCardToUse()).setText("");
+					getCardButtons().get(getCardToUse()).setVisible(false);
+					setCardToUse(-1);	
 			}
 			else if(card instanceof Skill){
 				Skill skillCard = (Skill) card;
 				if("weak vulnerable poison frail".contains(skillCard.getAttribute())){
 					if (!skillCard.requiresTarget()){
-						skillCard.use(player,combatMonsters);
+						skillCard.use(getPlayer(),getCombatMonsters());
 					}
 					else{
-						skillCard.use(player,combatMonsters[monsterIndex]);
+						skillCard.use(getPlayer(),getCombatMonsters()[monsterIndex]);
 					}
-					cardButtons.get(cardToUse).setText("");
-					cardButtons.get(cardToUse).setVisible(false);
-					cardToUse = -1;					
+					getCardButtons().get(getCardToUse()).setText("");
+					getCardButtons().get(getCardToUse()).setVisible(false);
+					setCardToUse(-1);					
 				}
 				else{
-					descriptions.setText("Invalid target");
-					cardToUse = -1;				
+					getDescriptions().setText("Invalid target");
+					setCardToUse(-1);				
 				}
 			}
 			else {
-				descriptions.setText("Invalid target");
-				cardToUse = -1;
+				getDescriptions().setText("Invalid target");
+				setCardToUse(-1);
 			}
 		}
 		
-		if (potionToUse >= 0){
-			Card potion = player.getPotions().get(potionToUse);
+		if (getPotionToUse() >= 0){
+			Card potion = getPlayer().getPotions().get(getPotionToUse());
 			if (potion.getDamage()>0){
 				if (!potion.requiresTarget()){
-					potion.use(player,combatMonsters);
+					potion.use(getPlayer(),getCombatMonsters());
 				}
 				else{
-					potion.use(player,combatMonsters[monsterIndex]);
+					potion.use(getPlayer(),getCombatMonsters()[monsterIndex]);
 				}
-				player.removePotion(potionToUse);
-				potionToUse = -1;
+				getPlayer().removePotion(getPotionToUse());
+				setPotionToUse(-1);
 			}
 			else if(potion instanceof Skill){
 				Skill skillPotion = (Skill) potion;
 				if("weak vulnerable poison frail".contains(skillPotion.getAttribute())){
 					if (!skillPotion.requiresTarget()){
-						skillPotion.use(player,combatMonsters);
+						skillPotion.use(getPlayer(),getCombatMonsters());
 					}
 					else{
-						skillPotion.use(player,combatMonsters[monsterIndex]);
+						skillPotion.use(getPlayer(),getCombatMonsters()[monsterIndex]);
 					}
-					player.removePotion(potionToUse);
-					potionToUse = -1;				
+					getPlayer().removePotion(getPotionToUse());
+					setPotionToUse(-1);				
 				}
 				else{
-					descriptions.setText("Invalid target");
-					potionToUse = -1;				
+					getDescriptions().setText("Invalid target");
+					setPotionToUse(-1);				
 				}
 			}
 			else {
-				descriptions.setText("Invalid target");
-				potionToUse = -1;
+				getDescriptions().setText("Invalid target");
+				setPotionToUse(-1);
 			}
 		}
 		GameGUI.refreshVisuals();
