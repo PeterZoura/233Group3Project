ff81cd641f3c9411526ccf95d1fecda7637d08fb
kweestor <47364487+kweestor@users.noreply.github.com>
2019-04-11 11:29:43 -0600

    Commented

--- buttonHandlers/CardButtonClick.java
+++ buttonHandlers/CardButtonClick.java
@@ -5,13 +5,16 @@ import javafx.event.EventHandler;
 import logic.Card;
 
 /**
-This class handles a button click and outputs a message.
-The handle method is invoked when the button is clicked.
+*This class handles a button click and outputs a message.
+*The handle method is invoked when the button is clicked.
 */
 public class CardButtonClick extends GameGUI implements EventHandler<ActionEvent>
 {
-	int cardIndex;
-
+	int cardIndex;//represents the index, in the array of cards, of the card that has been clicked
+	/**
+	*CardButtonClick detects when a card has been selected
+	*@param int cardIndex, represents the index of the card that has been click in the array of cards 
+	*/
 	public CardButtonClick(int cardIndex)
 	{
 		this.cardIndex = cardIndex;
@@ -30,7 +33,6 @@ public class CardButtonClick extends GameGUI implements EventHandler<ActionEvent
 			setCardToUse(-1);
 		}
 		setPotionToUse(-1);
-		//GameGUI.refreshVisuals();
     }
 }
 
